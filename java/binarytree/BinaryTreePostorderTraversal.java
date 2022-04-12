aec6a7310cb26a6271cf808fc458b83b8cd3b697
Praroop Gupta <praroop.gupta193@gmail.com>
2022-04-12 20:04:59 +0800

    Binary Tree: Basics

---
+++ java/binarytree/BinaryTreePostorderTraversal.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> ans = new ArrayList<>();
+        if(root != null){
+           postOrder(root, ans);
+        }
+        return ans;
+    }
+    
+    void postOrder(TreeNode root, List<Integer> list){
+        if(root == null)
+            return;
+        postOrder(root.left, list);
+        postOrder(root.right, list);
+        list.add(root.val);
+    }
+}
