81bcc93112578351fe8a6096c1e85cd2c02ae958
Praroop Gupta <praroop.gupta193@gmail.com>
2022-04-30 00:15:05 +0800

    Problems: Easy

---
+++ java/problems/ReshapeTheMatrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        if(m == r && n == c) {
+            return mat;
+        }
+        if(m*n != r*c){
+            return mat;
+        }
+        int[][] ans = new int[r][c];
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                list.add(mat[i][j]);
+            }
+        }
+        int k=0;
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                ans[i][j] = list.get(k);
+                k++;
+            }
+        }
+        return ans;
+    }
+}
