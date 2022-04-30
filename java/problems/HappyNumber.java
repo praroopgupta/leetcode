8f344a5bbca6397d17aad392009589fb36040989
Praroop Gupta <praroop.gupta193@gmail.com>
2022-05-01 00:10:07 +0800

    Problems: Easy

---
+++ java/problems/HappyNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+    public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+}
