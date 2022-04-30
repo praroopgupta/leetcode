9e78b005414bcb335af8c09509b3732b1f02cd7d
Praroop Gupta <praroop.gupta193@gmail.com>
2022-04-30 18:34:02 +0800

    Problems: Easy

---
+++ sql/problems/EmployeesWithMissingInformation.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select employee_id 
+from (select employee_id from Employees 
+     union all
+     select employee_id from Salaries) temp
+group by employee_id
+having count(employee_id) = 1
+order by employee_id asc;
