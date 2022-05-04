2c2c21ac9d0a0408a9a6ff2e05175dcecf3c3987
Praroop Gupta <praroop.gupta193@gmail.com>
2022-05-04 19:54:49 +0800

    Problems: Easy

---
+++ sql/problems/TheLatestLoginIn2020.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select user_id, max(time_stamp) as last_stamp from logins where time_stamp between '2020-01-01' and '2021-01-01' group by user_id order by user_id;
