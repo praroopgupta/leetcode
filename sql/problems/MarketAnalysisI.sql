e09e5b097fb35d8142a2b48e8606c1121e9b04a8
Praroop Gupta <praroop.gupta193@gmail.com>
2022-05-05 23:17:29 +0800

    Problems: Medium

---
+++ sql/problems/MarketAnalysisI.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select u.user_id as buyer_id, u.join_date, sum(case when o.order_id is not null then 1 else 0 end) as orders_in_2019 from users as u left join orders as o on u.user_id = o.buyer_id and year(o.order_date) = 2019
+group by u.user_id, u.join_date
+order by o.buyer_id
