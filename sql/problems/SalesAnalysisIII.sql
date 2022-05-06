bea1e391c3939e1d831cb07cbf25b691884ba9c1
Praroop Gupta <praroop.gupta193@gmail.com>
2022-05-06 23:09:42 +0800

    Problems: Easy

---
+++ sql/problems/SalesAnalysisIII.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select p.product_id, p.product_name from product as p inner join sales as s on p.product_id = s.product_id group by p.product_id having sum(s.sale_date between '2019-01-01' and '2019-03-31') = count(s.sale_date);
